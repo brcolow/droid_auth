514496aaecf74e2059845d4bfaedf680e781af31
Michael Ennen <mike@cryptodirectllc.com>
2015-08-09 13:38:03 -0700

    Add new logo - cryptodash icon with lock

--- app/src/main/java/net/cryptodirect/authenticator/ManualEntryFragment.java
+++ app/src/main/java/net/cryptodirect/authenticator/ManualEntryFragment.java
@@ -1,27 +1,40 @@
 package net.cryptodirect.authenticator;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ManualEntryFragment extends Fragment
 {
+    private static final String KEY_FONT_PATH = "fonts/Anonymous_Pro.ttf";
+
     public ManualEntryFragment()
     {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state)
     {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manual_entry, container, false);
+        View view = inflater.inflate(R.layout.fragment_manual_entry, container, false);
+
+        Typeface anonymousProTypeface = Typeface.createFromAsset(getActivity().getAssets(), KEY_FONT_PATH);
+
+        TextView emailField = (TextView) view.findViewById(R.id.email_field);
+        // we set the email field to anonymous pro for consistency with key text field
+        emailField.setTypeface(anonymousProTypeface);
+
+        TextView keyTextField = (TextView) view.findViewById(R.id.key_text_field);
+        keyTextField.setTypeface(anonymousProTypeface);
+
+        return view;
     }
 
 }
